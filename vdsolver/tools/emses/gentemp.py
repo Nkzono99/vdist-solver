1de09f16b6d65b5290c2704714f06b2c20f803e1
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2021-06-06 17:11:01 +0900

    Add: 1-dim velocity distribution solver template

--- vdsolver/tools/emses/gentemp.py
+++ vdsolver/tools/emses/gentemp.py
@@ -7,13 +7,57 @@ def parse_args():
 
     parser.add_argument('axises', default='xz')
     parser.add_argument('--output', '-o', default='vdist-solver.py')
+    parser.add_argument('--type', '-t', default='vdsolver')
 
     return parser.parse_args()
 
 
 def gentemp():
     args = parse_args()
+    if args.type == 'vdsolver':
+        gentemp_vdsolver(args)
+    elif args.type == 'backtrace':
+        gentemp_backtrace(args)
+
+
+def gentemp_vdsolver(args):
     chars = list(args.axises)
+    if len(chars) == 1:
+        gentemp_vdsolver1d(args, chars)
+    elif len(chars) == 2:
+        gentemp_vdsolver2d(args, chars)
+
+
+def gentemp_vdsolver1d(args, chars):
+    c1,  = chars
+    C1 = c1.upper()
+
+    filepath = Path(__file__).parent.parent.parent / \
+        'templates/vdist-solver1d.py.tmp'
+    with open(filepath, 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    line = 'v{}lim = {}'
+    lim_str = line.format(
+        'x', '[-1.0, 1.0, NX]' if 'x' in chars else '[0.0, 0.0, 1]')
+    lim_str += '\n    '
+    lim_str += line.format('y',
+                           '[-1.0, 1.0, NY]' if 'y' in chars else '[0.0, 0.0, 1]')
+    lim_str += '\n    '
+    lim_str += line.format('z',
+                           '[-1.0, 1.0, NZ]' if 'z' in chars else '[0.0, 0.0, 1]')
+
+    new = text.format(
+        C1=C1,
+        i1=['x', 'y', 'z'].index(c1),
+        lim=lim_str,
+    )
+
+    with open(args.output, 'w', encoding='utf-8') as f:
+        f.write(new)
+
+
+def gentemp_vdsolver2d(args, chars):
     c1, c2 = chars
     C1, C2 = c1.upper(), c2.upper()
 
@@ -42,3 +86,7 @@ def gentemp():
 
     with open(args.output, 'w', encoding='utf-8') as f:
         f.write(new)
+
+
+def gentemp_backtrace(args):
+    pass
