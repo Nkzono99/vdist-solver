ffdc9ada2d3efe7c7b11e34c6745a1ac98e52ad1
Nkzono99 <1735112t@gsuite.kobe-u.ac.jp>
2022-12-23 03:55:22 +0900

    Update: Implementing the Buneman-Boris method.

--- vdsolver/sims/essimulator.py
+++ vdsolver/sims/essimulator.py
@@ -22,6 +22,7 @@ class ESSimulator3d(Simulator):
                  nz: int,
                  dx: float,
                  ef: FieldVector3d,
+                 bf: FieldVector3d,
                  boundary_list: BoundaryList):
         super().__init__(boundary_list)
         self.nx = nx
@@ -29,6 +30,7 @@ class ESSimulator3d(Simulator):
         self.nz = nz
         self.dx = dx
         self.ef = ef
+        self.bf = bf if bf else lambda pos: np.zeros(3)
 
     def _apply_boundary(self, pcl: Particle) -> Particle:
         px, py, pz = pcl.pos
@@ -41,7 +43,27 @@ class ESSimulator3d(Simulator):
 
     def _backward(self, pcl: ChargedParticle, dt: float) -> ChargedParticle:
         pos_new = pcl.pos - dt * pcl.vel
-        vel_new = pcl.vel - dt * pcl.q_m * self.ef(pcl.pos)
+
+        # Update velocity by Buneman-Boris.
+        mdt2 = -0.5*dt
+        bf = self.bf(pcl.pos)
+        ef = self.ef(pcl.pos)
+
+        t = bf*pcl.q_m*mdt2
+        s = 2*t/(1 + t**2)
+
+        # Accerarate by e-field
+        upm = pcl.vel + pcl.q_m*ef*mdt2
+
+        # Rotate by b-field
+        upa = upm + np.cross(upm, t)
+        upp = upm + np.cross(upa, s)
+
+        # Accerarate by e-field
+        vel_new = upp + pcl.q_m*ef*mdt2
+
+        # Create new particle.
         t_new = pcl.t + dt
         pcl_new = ChargedParticle(pos_new, vel_new, q_m=pcl.q_m, t=t_new)
+
         return pcl_new
